162f14ee562db796c42692000845023054bc2272
Igor Fernandes <igor.fernandes@accenture.com>
2019-07-16 21:40:38 -0300

    Update to navigation

---
+++ app/src/main/java/br/com/if91wallet/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.if91wallet.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.if91wallet.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
